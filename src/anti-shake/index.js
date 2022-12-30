206cca04057eb2bd70a7d78576c9c44d5f67c140
雾恋 <zhuhong317626@163.com>
2022-12-30 17:07:38 +0800

    :ocean: 深拷贝以及防抖

---
+++ src/anti-shake/index.js
@@ -0,0 +1,29 @@
+
+/**
+ * 防抖
+ * @param {*} fn 函数 
+ * @param {*} time 时间 
+ * @returns 
+ */
+
+let timer = null
+
+const antiShake = function (foo) {
+
+    // 如果有时间就清除
+    if (timer) clearTimeout(timer);
+
+    shake(foo);
+};
+
+const shake = function ({ fn, time = 500}) {
+    const context = this;
+    let ages = arguments;
+    timer = setTimeout(() => {
+        // 在定时器里面调用需要 防抖的函数
+        fn.call(context, ages)
+    }, time);
+}
+
+
+export default antiShake;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import character from "./character/index";
 import sortSize from "./sort/size";
 import sort from "./sort/index";
 import deepClone from "./deep-clone/index";
+import antiShake from "./anti-shake/index";
 
 export default {
     meAscii:ascii,
     meCharacterSize:character,
     meSortSize:sortSize,
     meSort:sort,
-    meDeepClone:deepClone
+    meDeepClone:deepClone,
+    meAntiShake:antiShake
 }
