c0ed534065d8e792a573bac2ca7042686e370f4e
雾恋 <zhuhong317626@163.com>
2022-12-30 12:00:20 +0800

    深拷贝

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import ascii from "./ascii/index";
 import character from "./character/index";
 import sortSize from "./sort/size";
 import sort from "./sort/index";
+import deepCopy from "./deep-copy/index";
 
 export default {
     meAscii:ascii,
     meCharacterSize:character,
     meSortSize:sortSize,
-    meSort:sort
+    meSort:sort,
+    meDeepCopy:deepCopy
 }
